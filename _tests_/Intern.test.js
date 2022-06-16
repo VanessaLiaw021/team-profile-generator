e2b1275c165efaaba35d1e3018ffe5514dee6b03
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-16 15:22:53 -0400

    created a test case for setting the school name for Intern.test.js

--- _tests_/Intern.test.js
+++ _tests_/Intern.test.js
@@ -6,3 +6,19 @@ const testSchool = "UNCC";
 const testRole = "Intern";
 
 //Test cases for Intern class
+describe("Intern", () => {
+
+    //Test to set the constructor argument
+    describe("Initialization", () => {
+
+        //Test for setting school name
+        it("Should set the school name in constructor argument to be school name", () => {
+
+            //Create test instance 
+            const intern = new Intern("Lucy", 3, "testEmail@gmail.com", testSchool);
+
+            //Expect to be school name 
+            expect(intern.school).toBe(testSchool);
+        });
+    });
+});
