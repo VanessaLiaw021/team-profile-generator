821841b2e594f8c07f1bd604ef09422be32830b2
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-15 22:04:01 -0400

    created generateTeam() to generate each card for each employee, created a nested function for layout of manager card

--- src/template.js
+++ src/template.js
@@ -27,5 +27,35 @@ const createMarkupHTML = (markup) => {
     `);
 };
 
+//Function that create each card for each employee and display it to the html page
+const generateTeams = (teamData) => {
+
+    //Store each of the employee created card to the array 
+    let teamResults = [];
+
+    //Function that create the manager's card 
+    const generateManagerCard = (manager) => {
+        
+        //Layout of the manager card
+        const managerCard = `
+        <!--Card for manager-->
+        <div class="card m-3 bg-light" style="width: 18rem;">
+            <div class="card-header bg-primary text-light">
+                <h2 class="text-center pt-2">${manager.getName()}</h2>
+                <h3 class="text-center"><i class="fa-solid fa-mug-hot"></i>${manager.getRole()}</h3>
+            </div>
+            <ul class="list-group list-group-flush p-3">
+                <li class="list-group-item border">ID: ${manager.getId()}</li>
+                <li class="list-group-item border">Email: <a href="mailto: ${manager.getEmail()}" target="_blank">${manager.getEmail()}</a></li>
+                <li class="list-group-item border">Office Number: ${manager.getOfficeNumber()}</li>
+            </ul>
+        </div>
+        `;
+
+        //Push the manager card to the array 
+        teamResults.push(managerCard);
+    };
+};
+
 //Export createMarkupHTML() function
 module.exports = createMarkupHTML;
